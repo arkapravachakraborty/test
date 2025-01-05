62cbf39f00c70c30b6d8251766823d921ff94afc
Arkaprava Chakraborty <arkaprava200212345@gmail.com>
2025-01-05 15:12:37 +0530

    Add files via upload

---
+++ CC4_ArkapravaChakraborty_536_odd.py
@@ -0,0 +1,14 @@
+n = 50
+element = 0
+sum = 0
+print("Elements are:")
+while n:
+    if element % 2!= 0:
+        print(element, end = ', ')
+        sum += element
+        element += 1
+        n -= 1
+    else:
+        element += 1
+
+print(f"\n\n Sum of first 50 odd numbers is:", sum)
